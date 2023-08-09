6974d95ce1ec19e960316b23ac83854576aa9692
LDM <logandhi64@gmail.com>
2023-08-08 21:00:47 -0400

    Cleaned up dev-mode page

--- developer_mode.js
+++ developer_mode.js
@@ -1,14 +1,27 @@
+function createButton(text, method) {
+	const button = document.createElement("button");
+	button.innerHTML = text;
+	button.setAttribute("onclick", method);
+
+	document.getElementById("buttonPanel").appendChild(button);
+}
+
 function prn() {
+	showAlert(`Local storage is visible in the <b>console</b>.`)
 	console.log(window.localStorage);
 }
 
 function add() {
 	var rand = Math.round(Math.random()*100);
-	console.log(`Marking achievement #${rand} as completed`)
+	showAlert(`Marking achievement #${rand} as completed.`)
 	completeAchievement(rand);
 }
 
 function del() {
 	delete window.localStorage.completed_achievements;
-	showAlert("Cleared local storage");
-} 
+	showAlert("All items in local storage have been permanently deleted.");
+}
+
+window.onload = function() {
+	showAlert("This page was created for development purposes! If you don't know what you're doing, <b>GO BACK!</b> It is possible to erase <b>ALL YOUR DATA</b> from this page!")
+};
