1247c00b1707f9dde28a3233d24a795f419d3f5f
LDM <logandhi64@gmail.com>
2023-08-09 18:28:08 -0400

    Fixed checkboxes, completion now stored in local

--- achievement_guide.js
+++ achievement_guide.js
@@ -1,3 +1,6 @@
+const uncheckedIcon = "fa-regular fa-square achievement-completed-icon";
+const checkedIcon = "fa-solid fa-square-check achievement-completed-icon"
+
 async function fetchAchievementsList() {
 	return fetch("./assets/data/achievements.json")
 			.then(function(data) {
@@ -35,9 +38,13 @@ async function createAchievement() {
 	const rarity = document.createElement("div");
 	rarity.className = "achievement-rarity gray";
 
-	const checkbox = document.createElement("input");
-	checkbox.type = "checkbox";
+	const checkbox = document.createElement("button");
 	checkbox.className = "achievement-completed";
+
+	const checkboxIcon = document.createElement("i");
+	checkboxIcon.className = "fa-regular fa-square achievement-completed-icon";
+
+	checkbox.appendChild(checkboxIcon);
 
 	reward.appendChild(trophyIcon);
 	reward.appendChild(document.createTextNode("\u00A0"));
@@ -61,6 +68,11 @@ function calcRewardColor(xp) {
 	else return "gold";
 }
 
+function isAchievementComplete(id) {
+	if (window.localStorage.completed_achievements == undefined) window.localStorage.completed_achievements = "[]"
+	return JSON.parse(window.localStorage.completed_achievements).includes(id);
+}
+
 async function loadAchievement(cardIndex, achievement) {
 	document.getElementsByClassName("achievement-icon")[cardIndex].src = `assets/achievements/${achievement.title}.png`;
 	document.getElementsByClassName("achievement-title")[cardIndex].innerHTML = achievement.title;
@@ -68,7 +80,16 @@ async function loadAchievement(cardIndex, achievement) {
 	document.getElementsByClassName("achievement-reward")[cardIndex].className = `achievement-reward ${calcRewardColor(achievement.xp)}`;
 	document.getElementsByClassName("achievement-xp")[cardIndex].innerHTML = `${achievement.xp} XP`;
 	document.getElementsByClassName("achievement-rarity")[cardIndex].innerHTML = `${achievement.rarity}% of player have this achievement`;
-	document.getElementsByClassName("achievement-completed")[cardIndex].id = `achievement-${achievement.id}`;
+
+	const _checkbox = document.getElementsByClassName("achievement-completed")[cardIndex];
+	const _complete = isAchievementComplete(achievement.id);
+	_checkbox.id = `achievement-${achievement.id}`;
+	_checkbox.setAttribute("onclick", `onAchievementCheckboxClicked(${achievement.id})`);
+	_checkbox.setAttribute("data-checked", _complete);
+
+	const _icon = document.getElementsByClassName("achievement-completed-icon")[cardIndex];
+	_icon.id = `achievement-icon-${achievement.id}`;
+	_icon.className = _complete ? checkedIcon : uncheckedIcon;
 }
 
 const promisedAchievements = fetchAchievementsList();
@@ -87,15 +108,38 @@ async function updateAchievements() {
 function completeAchievement(id) {
     if (window.localStorage.completed_achievements == undefined) window.localStorage.completed_achievements = "[]"
     let completedAchievements = JSON.parse(window.localStorage.completed_achievements);
+
     completedAchievements.push(id);
-    window.localStorage.completed_achievements = JSON.stringify(completedAchievements);
+
+    window.localStorage.completed_achievements = JSON.stringify([...new Set(completedAchievements)]);
 }
 
 function uncompleteAchievement(id) {
     if (window.localStorage.completed_achievements == undefined) window.localStorage.completed_achievements = "[]"
     let completedAchievements = JSON.parse(window.localStorage.completed_achievements);
-    completedAchievements.pop(id);
-    window.localStorage.completed_achievements = JSON.stringify(completedAchievements);
+
+	if (!completedAchievements.includes(id)) {
+		console.warn("Achievement uncompleted, but achievement is not complete in the first place. Ignoring.");
+		return;
+	}
+
+    completedAchievements = completedAchievements.filter(val => val !== id)
+    window.localStorage.completed_achievements = JSON.stringify([...new Set(completedAchievements)]);
+}
+
+function onAchievementCheckboxClicked(id) {
+	const checkbox = document.getElementById(`achievement-${id}`);
+	const icon = document.getElementById(`achievement-icon-${id}`);
+
+	if (checkbox.getAttribute("data-checked") == "true") {
+		uncompleteAchievement(id);
+		icon.className = uncheckedIcon;
+		checkbox.setAttribute("data-checked", "false");
+	} else {
+		completeAchievement(id);
+		icon.className = checkedIcon;
+		checkbox.setAttribute("data-checked", "true");
+	}
 }
 
 window.onload = function() {
