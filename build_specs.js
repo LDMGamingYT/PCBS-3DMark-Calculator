6540317307d030893e11225ab91565c14c1541d0
LDM <logandhi64@gmail.com>
2023-08-09 21:35:58 -0400

    Fixed drive buttons

--- build_specs.js
+++ build_specs.js
@@ -1,5 +1,31 @@
 function addDrive() {
-	console.log("please dont reload")
+	const parent = document.getElementById("storage-drives");
+	const _thisDriveIndex = parseInt(parent.getAttribute("data-drives")) + 1;
+
+	const icon = document.createElement("i");
+	icon.className = "fa-solid fa-hard-drive";
+
+	const header = document.createElement("span");
+	header.appendChild(icon);
+	header.appendChild(document.createTextNode(` Drive #${_thisDriveIndex}`));
+
+	const input = document.createElement("input");
+	input.name = `storage`;
+	input.id = `storage-${_thisDriveIndex}`;
+	input.list = "storageDriveList";
+	input.value = document.getElementById(`storage-${_thisDriveIndex-1}`).value;
+
+	parent.setAttribute("data-drives", _thisDriveIndex)
+
+	parent.appendChild(header);
+	parent.appendChild(input);
+}
+
+function removeDrive() {
+	const parent = document.getElementById("storage-drives");
+	const _driveIndex = parseInt(parent.getAttribute("data-drives"));
+
+	parent.removeChild(document.getElementById(`storage-${_driveIndex}`))
 }
 
 function calculateSpecs() {
