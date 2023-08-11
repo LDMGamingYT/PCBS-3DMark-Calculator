7bd2543e782bbd7bff3f2de29aa72e5ff5ff064b
LDM <logandhi64@gmail.com>
2023-08-11 16:51:27 -0400

    Added `ms` parameter to `showAlert`

--- global.js
+++ global.js
@@ -1,8 +1,8 @@
-function showAlert(message) {
+function showAlert(message, ms = 2500) {
 	var alertBox = document.getElementById("alertBox");
 	document.getElementById("alertMessage").innerHTML = message;
 	alertBox.setAttribute("data-active", "true");
 	setTimeout(function() {
 		alertBox.setAttribute("data-active", "false")
-	}, 2500);
+	}, ms);
 }
